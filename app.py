7697a0856617025a9f9da0157eba7b9bbc0161bd
kamalahasitha <117288193+kamalahasitha@users.noreply.github.com>
2023-07-19 20:23:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -60,9 +60,96 @@ def get_students():
         for student in students
     ]
     return jsonify(result)
+@app.route('/student/<student_id>', methods=['GET'])
+
+def get_student(student_id):
+
+    student = Student.query.filter_by(student_id=student_id).first()
+    if not student:
+
+        return jsonify({'message': 'Student not found'}), 404
 
 
 
+
+    result = {
+
+        'student_id': student.student_id,
+
+        'first_name': student.first_name,
+
+        'last_name': student.last_name,
+
+        'dob': student.dob.isoformat(),
+
+        'amount_due': student.amount_due
+
+    }
+
+    return jsonify(result)
+@app.route('/student/<student_id>', methods=['PUT'])
+
+def update_student(student_id):
+
+    student = Student.query.filter_by(student_id=student_id).first()
+
+
+
+
+    if not student:
+
+        return jsonify({'message': 'Student not found'}), 404
+
+
+
+
+    data = request.get_json()
+
+    student.first_name = data.get('first_name', student.first_name)
+
+    student.last_name = data.get('last_name', student.last_name)
+
+    student.dob = data.get('dob', student.dob)
+
+    student.amount_due = data.get('amount_due', student.amount_due)
+
+
+
+
+    db.session.commit()
+
+
+
+
+    return jsonify({'message': 'Student updated successfully'})
+
+
+
+
+@app.route('/student/<student_id>', methods=['DELETE'])
+
+def delete_student(student_id):
+
+    student = Student.query.filter_by(student_id=student_id).first()
+
+
+
+
+    if not student:
+
+        return jsonify({'message': 'Student not found'}), 404
+
+
+
+
+    db.session.delete(student)
+
+    db.session.commit()
+
+
+
+
+    return jsonify({'message': 'Student deleted successfully'})
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
